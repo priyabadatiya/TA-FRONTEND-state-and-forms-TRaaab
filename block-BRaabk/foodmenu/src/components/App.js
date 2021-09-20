0dd8ba30d28911104bc2715b71d6ea1ccacfac2b
priyabadatiya <priyabadatiya@gmail.com>
2021-09-21 01:44:08 +0530

    initial

---
+++ block-BRaabk/foodmenu/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'React';
+import data from '../data.json';
+
+class App extends React.Component{
+constructor(props) {
+  super(props);
+}
+render(){
+
+}
+
+}
+
+
+export default App;
