dbe8d7d9508db8cfaf7bb5f744463a022dfd79d4
priyabadatiya <priyabadatiya@gmail.com>
2021-09-28 01:40:07 +0530

    todo.md

--- block-BRaabv/multi-form/src/components/Checkbox.js
+++ block-BRaabv/multi-form/src/components/Checkbox.js
@@ -1,4 +1,33 @@
 import React from 'react';
-import Signup from './Signup';
-import Message from './Message';
-import Checkbox from './Checkbox';
+
+class Step3 extends React.Component {
+
+    constructor(props) {
+        super(props);
+       
+    }
+    render() {
+        
+        if(this.props.currentStep !== 3) {
+            return null;
+        }
+        return (
+                
+                <>
+                   
+                    <legend className="text-3xl text-blue-900 font-bold my-4">Checkbox</legend>
+                    
+                            <label className="text-gray-900 block" htmlFor="gender">Gender</label>
+                            <select className="py-2 px-6 rounded-lg block my-2 form-control"id="gender" onChange={this.props.handleChange} value={this.props.select} name="select">
+                            <option value="Select an option" defaultValue hidden>Select an option</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                            </select>
+                </>
+            
+        )
+    }
+}
+
+export default Step3;
